1db8acf7e60ec99f9713758bc076a51878e7aa22
yasutaka.imaeda <yasu89baka1013@icloud.com>
2022-02-04 21:29:29 +0900

    amplifyの初期化完了

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,9 @@ import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import Amplify from "aws-amplify";
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
 
 ReactDOM.render(
   <React.StrictMode>
