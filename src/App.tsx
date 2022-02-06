125118f9cd50a6f07b2763ae78db0f25c404764f
yasutaka.imaeda <yasu89baka1013@icloud.com>
2022-02-06 12:31:44 +0900

    認証とDBの作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,105 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, { useEffect, useState } from "react";
+// import logo from "./logo.svg";
+// import { Counter } from "./features/counter/Counter";
+import "./App.css";
+/* src/App.js */
+import Amplify, { API, graphqlOperation } from "aws-amplify";
+import { createTodo } from "./mutations";
+import { listTodos } from "./queries";
+
+import awsExports from "./aws-exports";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+Amplify.configure(awsExports);
+
+const initialState = { name: "", description: "" };
 
 function App() {
+  const [formState, setFormState] = useState(initialState);
+  const [todos, setTodos] = useState<any>([]);
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const setInput = (key: any, value: any) => {
+    setFormState({ ...formState, [key]: value });
+  };
+
+  const fetchTodos = async () => {
+    try {
+      const todoData: any = await API.graphql(graphqlOperation(listTodos));
+      const todos = todoData.data.listTodos.items;
+      setTodos(todos);
+    } catch (err) {
+      console.log("error fetching todos");
+    }
+  };
+
+  const addTodo = async () => {
+    try {
+      if (!formState.name || !formState.description) return;
+      const todo: any = { ...formState };
+      setTodos([...todos, todo]);
+      setFormState(initialState);
+      await API.graphql(graphqlOperation(createTodo, { input: todo }));
+    } catch (err) {
+      console.log("error creating todo:", err);
+    }
+  };
+
+  const styles: any = {
+    container: {
+      width: 400,
+      margin: "0 auto",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      padding: 20,
+    },
+    todo: { marginBottom: 15 },
+    input: {
+      border: "none",
+      backgroundColor: "#ddd",
+      marginBottom: 10,
+      padding: 8,
+      fontSize: 18,
+    },
+    todoName: { fontSize: 20, fontWeight: "bold" },
+    todoDescription: { marginBottom: 0 },
+    button: {
+      backgroundColor: "black",
+      color: "white",
+      outline: "none",
+      fontSize: 18,
+      padding: "12px 0px",
+    },
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div style={styles.container}>
+      <h2>Amplify Todos</h2>
+      <input
+        onChange={(event) => setInput("name", event.target.value)}
+        style={styles.input}
+        value={formState.name}
+        placeholder="Name"
+      />
+      <input
+        onChange={(event) => setInput("description", event.target.value)}
+        style={styles.input}
+        value={formState.description}
+        placeholder="Description"
+      />
+      <button style={styles.button} onClick={addTodo}>
+        Create Todo
+      </button>
+      {todos.map((todo: any, index: any) => (
+        <div key={todo.id ? todo.id : index} style={styles.todo}>
+          <p style={styles.todoName}>{todo.name}</p>
+          <p style={styles.todoDescription}>{todo.description}</p>
+        </div>
+      ))}
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
